f40ac56ce1c3f6a346ae5e9f05649ef9e1943521
nakinjoti01-wq <nakinjoti01@gmail.com>
2026-09-10 00:27:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -667,7 +667,7 @@ else:
 
             if yolo_model:
                 results = yolo_model.predict(frame, conf=0.25, device="cpu", verbose=False)
-                    bike_boxes = []
+            bike_boxes = []
                     st.session_state["spatial_density_accumulator"] *= heatmap_decay
 
                     for box in results[0].boxes.xyxy.cpu().numpy():
